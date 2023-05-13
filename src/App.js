a5b10715f9a44c60eccddea6f00828e33af5b3b8
HongbeenLee <redbean@hutom.co.kr>
2023-05-14 00:40:24 +0900

    add eslint setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 export function replaceCamelWithSpace(colorName) {
   return colorName.replace(/\B([A-Z])\B/g, " $1");
 }
 
 function App() {
-  const [ buttonColor, setButtonColor ] = useState('MediumVioletRed');
+  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
   const [disabled, setDisabled] = useState(false);
-  
-  const newButtonColor = buttonColor === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed';
+
+  const newButtonColor =
+    buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
 
   return (
-    <div >
-      <button 
-        style={{backgroundColor: disabled ? "gray" : buttonColor}}
+    <div>
+      <button
+        style={{ backgroundColor: disabled ? "gray" : buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}
-        disabled={disabled}
-        >
+        disabled={disabled}>
         Change to {replaceCamelWithSpace(newButtonColor)}
       </button>
-      <label>Disabled button
-        <input type="checkbox" defaultChecked={disabled} onChange={(e) => setDisabled(e.target.checked)} />
+      <label>
+        Disabled button
+        <input
+          type="checkbox"
+          defaultChecked={disabled}
+          onChange={(e) => setDisabled(e.target.checked)}
+        />
       </label>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App, {replaceCamelWithSpace} from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App, { replaceCamelWithSpace } from "./App";
 
-test('button has correct initial color, and update color when clicked', () => {
+test("button has correct initial color, and update color when clicked", () => {
   render(<App />);
-  
+
   // find an element with a role of button and text of "Change to blue"
-  const button = screen.getByRole('button', {
-    name: `Change to Midnight Blue`
+  const button = screen.getByRole("button", {
+    name: `Change to Midnight Blue`,
   });
 
   // 모든 assertion은 expect로 시작한다. css을 가지고 있는지 확인한다.
@@ -16,35 +16,35 @@ test('button has correct initial color, and update color when clicked', () => {
 
   // button turns blue when clicked
   fireEvent.click(button);
-  
+
   expect(button).toHaveStyle(`
   background-color: MidnightBlue;
   `);
-  
+
   expect(button).toHaveTextContent("Change to Medium Violet Red");
 });
 
-test('checkbox, button initial condition.', () => {
+test("checkbox, button initial condition.", () => {
   render(<App />);
 
-  const button = screen.getByRole('button', {
-    name: /Change to/
+  const button = screen.getByRole("button", {
+    name: /Change to/,
   });
-  const checkbox = screen.getByRole('checkbox');
+  const checkbox = screen.getByRole("checkbox");
 
   // checkbox has checked as initial state, the button is enabled.
   expect(button).toBeEnabled();
   expect(checkbox).not.toBeChecked();
-})
+});
 
-test('when checkbox unchecked, button should be disabled.', () => {
+test("when checkbox unchecked, button should be disabled.", () => {
   render(<App />);
 
-  const button = screen.getByRole('button', {
-    name: /Change to/
+  const button = screen.getByRole("button", {
+    name: /Change to/,
   });
-  const checkbox = screen.getByRole('checkbox', {
-    name: "Disabled button"
+  const checkbox = screen.getByRole("checkbox", {
+    name: "Disabled button",
   });
 
   // when checkbox has checked, the button is disabled.
@@ -54,16 +54,16 @@ test('when checkbox unchecked, button should be disabled.', () => {
   // when checkbox has unchecked again, the button is enabled.
   fireEvent.click(checkbox);
   expect(button).toBeEnabled();
-})
+});
 
-test('버튼이 비활성회되면, disabled 색상을 보여주고 활성화되면 기존 색으로 돌아온다.', () => {
+test("버튼이 비활성회되면, disabled 색상을 보여주고 활성화되면 기존 색으로 돌아온다.", () => {
   render(<App />);
 
-  const button = screen.getByRole('button', {
-    name: /Change to/
+  const button = screen.getByRole("button", {
+    name: /Change to/,
   });
-  const checkbox = screen.getByRole('checkbox', {
-    name: "Disabled button"
+  const checkbox = screen.getByRole("checkbox", {
+    name: "Disabled button",
   });
 
   // 버튼 비활성화 -> 회색 확인 -> 활성화 -> 기존색으로 돌아오는지 확인
@@ -86,19 +86,19 @@ test('버튼이 비활성회되면, disabled 색상을 보여주고 활성화되
   expect(button).toHaveStyle(`
   background-color: MidnightBlue;
   `);
-})
+});
 
-describe('spaces before camel-case capital letters', () => {
-  test('Works for no inner capital letters', () => {
-    expect(replaceCamelWithSpace('Red')).toBe('Red');
+describe("spaces before camel-case capital letters", () => {
+  test("Works for no inner capital letters", () => {
+    expect(replaceCamelWithSpace("Red")).toBe("Red");
   });
-  test('Works for one inner capital letter', () => {
-    expect(replaceCamelWithSpace('MidnightBlue')).toBe('Midnight Blue');
+  test("Works for one inner capital letter", () => {
+    expect(replaceCamelWithSpace("MidnightBlue")).toBe("Midnight Blue");
   });
-  test('Works for multiple inner capital letters', () => {
-    expect(replaceCamelWithSpace('MediumVioletRed')).toBe('Medium Violet Red');
+  test("Works for multiple inner capital letters", () => {
+    expect(replaceCamelWithSpace("MediumVioletRed")).toBe("Medium Violet Red");
   });
-})
+});
 
 // 시작할때는 MediumVioletRed, 그리고 MidnightBlue로 바뀌는지
 // 원래는 red -> blue 였는데 스펙이 변경되었으니까 기존 테스트를 업데이트해야함
